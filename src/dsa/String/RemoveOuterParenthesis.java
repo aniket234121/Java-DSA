5618e29cbb2133f862a66a73c639770c6dcd4050
aniket chauhan <aniketchauhan329@gmail.com>
2024-09-21 11:30:19 +0530

    added removeOuterParenthesis Q.1021

--- src/dsa/String/RemoveOuterParenthesis.java
+++ src/dsa/String/RemoveOuterParenthesis.java
@@ -1,20 +1,66 @@
 package dsa.String;
 
-import java.util.Stack;
+import java.util.Arrays;
+//
+//A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+//
+//For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+//A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+//
+//Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+//
+//Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+//
+//
+//
+//        Example 1:
+//
+//Input: s = "(()())(())"
+//Output: "()()()"
+//Explanation:
+//The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+//After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+//Example 2:
+//
+//Input: s = "(()())(())(()(()))"
+//Output: "()()()()(())"
+//Explanation:
+//The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+//After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+//Example 3:
+//
+//Input: s = "()()"
+//Output: ""
+//Explanation:
+//The input string is "()()", with primitive decomposition "()" + "()".
+//After removing outer parentheses of each part, this is "" + "" = "".
 
 public class RemoveOuterParenthesis {
     public static String removeOuterParentheses(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(int i = 0; i < s.length(); i++) {
-
+        int count=0;
+        String sb="";
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(')
+            {
+                count++;
+                if(count>1)
+                {
+                    sb+=s.charAt(i);
+                }
+            }
+            else {
+                count--;
+                if(count>0) {
+                    sb += s.charAt(i);
+                }
+            }
         }
-
-        return s;
+        return sb;
     }
 
     public static void main(String[] args) {
-        String str="(()()(()))";
-
-        removeOuterParentheses(str);
+        String str="(()())(())(()(()))";
+        str=removeOuterParentheses(str);
+        System.out.println(Arrays.toString(str.toCharArray()));
     }
 }
